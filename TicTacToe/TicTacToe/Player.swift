ed925454abec2211c7ef4bc7ccd606ac7526baeb
Olof Hammar <olle_hammat@hotmail.com>
2020-12-18 15:41:12 +0100

    Added new comments.

--- TicTacToe/TicTacToe/Player.swift
+++ TicTacToe/TicTacToe/Player.swift
@@ -12,24 +12,30 @@ import Foundation
  and the brick variable is set the X for one player and O for the other in ViewController.
 */
 class Player {
+    
     var name: String
     var brick: String
     var wins: Int
     
     init(brick: String) {
+        
         self.name = "---"
         self.brick = brick
         self.wins = 0
     }
+    
     func updateName(newName: String) {
+        
         self.name = newName
     }
     
     func addWin()  {
+        
         wins += 1
     }
     
     func resetPlayerScore() {
+        
         wins = 0
     }
 }
